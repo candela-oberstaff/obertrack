3cd64e21c28e7341fa3bdafdbf300f52f0033699
candela-oberstaff <candela@oberstaff.com>
2025-12-03 16:07:05 -0300

    dashboard

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://i.postimg.cc/brHYgk8X/Obertrack-2.png" class="mt-5" alt="Logo de Mi Sitio Web" width="100" height="100">
+<img src="{{ asset('images/logo.png') }}" {{ $attributes }} alt="Obertrack Logo">
