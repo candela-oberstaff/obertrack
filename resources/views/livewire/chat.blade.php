ccdc243c413d88cd341e841a4741d6edccccd36d
candela-oberstaff <candela@oberstaff.com>
2025-12-30 12:31:15 -0300

    chat

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -138,7 +138,7 @@
             <div class="relative">
                 <input 
                     type="text" 
-                    wire:model.live.debounce.300ms="search"
+                    wire:model.live="search"
                     placeholder="Buscar contacto..." 
                     class="w-full pl-10 pr-4 py-2 bg-gray-50 border border-gray-200 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary transition-all"
                 >
